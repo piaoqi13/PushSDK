6a7b8b7a34a37bfbc043fd48b7c9023396a24bdb
piaoqi13@qq.com <123!@#asd>
2016-04-15 08:29:50 +0800

    友盟推送库工程

---
+++ src/com/umeng/pushsdk/MainActivity.java
@@ -0,0 +1,15 @@
+package com.umeng.pushsdk;
+
+import com.umeng.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
